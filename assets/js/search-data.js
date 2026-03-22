5d97a583ac31f9f0288a45342fed05cf603de27a
thesakshipandey <54810583+thesakshipandey@users.noreply.github.com>
2026-03-22 20:55:24 +0000

    Deploying to gh-pages from @ thesakshipandey/thesakshipandey.github.io@1ebf7277757c2d0bba6eb7791c0b913f20f161ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-blog",
-          title: "BLOG",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
